7667f1d5c9a9a6a5a0f3efc9a986151e20356e3c
Aleksandr Miasniankin <jivoy1057@mail.ru>
2023-09-29 20:05:11 +0300

    доделал меню моильное, адаптивы

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
   const burgerBtn = document.querySelector(".burger");
+  const mainNav = document.querySelector(".main-nav");
   burgerBtn.addEventListener("click", function () {
     burgerBtn.classList.toggle("active");
+    mainNav.classList.toggle('active');
   });
 
   const minViewPort = (min = 360) => {
@@ -25,12 +27,18 @@ document.addEventListener("DOMContentLoaded", function () {
     },
   });
 
-  const btnShowSubMenu = document.querySelector("#showMainSubmenu");
-  const wrapMainSubMenu = document.querySelector(".main-submenu");
-  btnShowSubMenu.addEventListener("mouseover", () => {
-    wrapMainSubMenu.classList.add("active");
-  });
-  wrapMainSubMenu.addEventListener("mouseleave", () => {
-    wrapMainSubMenu.classList.remove("active");
-  });
+  const showSubMenu = function () {
+    const btnShowSubMenu = document.querySelector("#showMainSubmenu");
+    const wrapMainSubMenu = document.querySelector(".main-submenu");
+    btnShowSubMenu.addEventListener("mouseover", () => {
+      wrapMainSubMenu.classList.add("active");
+    });
+    wrapMainSubMenu.addEventListener("mouseleave", () => {
+      wrapMainSubMenu.classList.remove("active");
+    });
+  };
+
+  if (window.innerWidth > 1024) {
+    showSubMenu();
+  }
 });
