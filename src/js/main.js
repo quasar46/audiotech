14b770c740fa93e07b0c71d71b2532dd1f2b9639
Aleksandr Miasniankin <jivoy1057@mail.ru>
2023-10-02 23:29:08 +0300

    доделан каталог, формы готовы на 99%

--- src/js/main.js
+++ src/js/main.js
@@ -62,26 +62,69 @@ document.addEventListener("DOMContentLoaded", function () {
   if (window.innerWidth > 1024) {
     showSubMenu();
   }
+
+  const btnsCloseWindow = document.querySelectorAll(".close-window");
+
+  btnsCloseWindow.forEach((btn) => {
+    btn.addEventListener("click", function (e) {
+      e.stopPropagation();
+      scrollLock.enablePageScroll();
+      btn.closest("div").classList.remove("active");
+      if (overlay.classList.contains("active")) {
+        overlay.classList.remove("active");
+      }
+    });
+  });
+
+  const showFilter = function () {
+    const btnShowFilter = document.querySelector("#showFilter");
+    const filterWindow = document.querySelector(".filter");
+    if (btnShowFilter !== null) {
+      btnShowFilter.addEventListener("click", function () {
+        filterWindow.classList.add("active");
+        scrollLock.disablePageScroll();
+      });
+    }
+  };
+
+  showFilter();
+
+  const overlay = document.querySelector("#overlay");
+  const btnsShowReg = document.querySelectorAll(".showModalReg");
+  const btnsShowCall = document.querySelectorAll(".showModalCall");
+  const modalReg = document.querySelector("#modalReg");
+  const modalAccept = document.querySelector('#modalAccept');
+  const modalCall = document.querySelector("#modalCall");
+
+  btnSubmitReg = document.querySelector('.modal__reg .btn');
+
+  btnSubmitReg.addEventListener('click', function(e) {
+    e.preventDefault();
+    modalReg.classList.remove('active');
+    modalAccept.classList.add('active');
+  })
+
+  btnsShowReg.forEach((btn) => {
+    btn.addEventListener("click", function (e) {
+      e.stopPropagation();
+      scrollLock.disablePageScroll();
+      overlay.classList.add("active");
+      modalReg.classList.add("active");
+    });
+  });
+
+  btnsShowCall.forEach((btn) => {
+    btn.addEventListener("click", function (e) {
+      e.stopPropagation();
+      scrollLock.disablePageScroll();
+      overlay.classList.add("active");
+      modalCall.classList.add("active");
+    });
+  });
+
+  customSelect("select");
+
+
+  new AirDatepicker('#input');
+
 });
-
-const btnsCloseWindow = document.querySelectorAll('.close-window');
-
-btnsCloseWindow.forEach(btn => {
-  btn.addEventListener('click', function(e) {
-    e.stopPropagation();
-    scrollLock.enablePageScroll();
-    btn.closest('div').classList.remove('active');
-  });
-})
-
-const showFilter = function () {
-  const btnShowFilter = document.querySelector('#showFilter');
-  const filterWindow = document.querySelector(".filter");
-  btnShowFilter.addEventListener("click", function () {
-    console.log(this);
-    filterWindow.classList.add("active");
-    scrollLock.disablePageScroll();
-  });
-};
-
-showFilter();
